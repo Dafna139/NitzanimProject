e57f8c365cc9f742e2e91f72f0247359b8c9c7c9
Dafna139 <dafna11211@gmail.com>
2023-08-30 11:45:59 +0300

    print hello everyone

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("HEllo bitches")
+# import
+results_dict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+def update_possible_diseases(symptom_from_the_user):
+    for disease in results_dict.keys():
+        if symptom_from_the_user not in results_dict.get(disease):
+            del results_dict[disease]
+
