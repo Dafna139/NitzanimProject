e4fd580a16f7d767e812b663f6c644eaaf02d329
Dafna139 <dafna11211@gmail.com>
2023-08-30 12:16:33 +0300

    *Wrote a new function to update the result list *Wrote a new function to check if a specific disease has a specific symptom

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def update_possible_diseases(symptom_from_the_user):
     for disease in DataAnalisys.results_dict.keys():
         if symptom_from_the_user not in DataAnalisys.results_dict.get(disease):
             del DataAnalisys.results_dict[disease]
+
+
+# The function checks and returns if a symptom is related to a specific disease
+def is_symptom_related_to_disease(symptom_from_the_user, disease):
+    symptoms_of_disease = DataAnalisys.results_dict.get(disease)
+    return symptom_from_the_user in symptoms_of_disease
