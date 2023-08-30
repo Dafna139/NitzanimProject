947e3e86f6bf071083325e4331989e4fba516e58
advaCozen <advacohen12345@gmail.com>
2023-08-30 12:21:10 +0300

    print hello

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+# def print_op(show_list):
+#     for key in show_list:
+#         print(f"{key} - {show_list[key]}")
+def print_op(list_value):
+    if len(list_value) < 3:
+        print(list_value, end=" ")
+        temp = list_value.copy()
+    else:
+        print(list_value[0:2], end=" ")
+        temp = list_value[0:2].copy()
+    return temp
 
-print("hello")
+
+
+
+
+
+
+
+dict1 = {"aaa":["bww","cq","d2","h11"], "bbbb":["e","r","t"]}
+show(dict1)
