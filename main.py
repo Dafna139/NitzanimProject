531029f198d1b1ad030ca7229906621b6116cac2
Dafna139 <dafna11211@gmail.com>
2023-08-30 11:58:26 +0300

    Wrote a new function to update the result list

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# import
-results_dict = {
-        "brand": "Ford",
-        "model": "Mustang",
-        "year": 1964
-    }
-def update_possible_diseases(symptom_from_the_user):
-    for disease in results_dict.keys():
-        if symptom_from_the_user not in results_dict.get(disease):
-            del results_dict[disease]
+import DataAnalisys
 
+
+# The function updates the results dict according to the new symptom
+def update_possible_diseases(symptom_from_the_user):
+    for disease in DataAnalisys.results_dict.keys():
+        if symptom_from_the_user not in DataAnalisys.results_dict.get(disease):
+            del DataAnalisys.results_dict[disease]
