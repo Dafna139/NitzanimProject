4336cca73062c47f293e9b3a5963840cb9687768
advaCozen <advacohen12345@gmail.com>
2023-08-30 12:34:20 +0300

    print hello

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-# def print_op(show_list):
-#     for key in show_list:
-#         print(f"{key} - {show_list[key]}")
+
 def print_op(list_value):
     if len(list_value) < 3:
         print(list_value, end=" ")
         temp = list_value.copy()
     else:
-        print(list_value[0:2], end=" ")
-        temp = list_value[0:2].copy()
+        print(list_value[0:3], end=" ")
+        temp = list_value[0:3].copy()
+    print()
+
+
+def get_list(list_value):
+    if len(list_value) < 3:
+        temp = list_value.copy()
+    else:
+        temp = list_value[0:3].copy()
     return temp
+def question(thisdict):
+    for name in thisdict:
+        print_op(thisdict[name])
+        user_input = input()
+        # func
+        flag = True
+        sym = get_list(thisdict[name])
+        if flag:
+            sym.remove(user_input)
+        print(sym)
+
+
+
+question({"aaa":["21","121"], "bbbb":["111","222","333","444"]})
 
 
 
 
-
-
-
-
-dict1 = {"aaa":["bww","cq","d2","h11"], "bbbb":["e","r","t"]}
-show(dict1)
