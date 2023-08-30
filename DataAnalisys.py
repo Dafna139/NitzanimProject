1c276d9d22e727a25440029962d88498b8fd2218
AlisaPh <a.arthopkins@gmail.com>
2023-08-30 12:33:03 +0300

    started analyse the data

--- DataAnalisys.py
+++ DataAnalisys.py
@@ -1,39 +1,20 @@
 # Python program to read an excel file
-
-# import openpyxl module
 import openpyxl
+import pandas as pd
 
-# Give the location of the file
-path = "dataset_symptoms.csv"
+PATH = "dataset_symptoms.xlsx"
 
-# To open the workbook
-# workbook object is created
-wb_obj = openpyxl.load_workbook(path)
 
-# Get workbook active sheet object
-# from the active attribute
-sheet_obj = wb_obj.active
+df = pd.read_excel(PATH)
+dict = df.to_dict()
 
-# Getting the value of maximum rows
-# and column
-row = sheet_obj.max_row
-column = sheet_obj.max_column
+#create symptom and disease list
+d_list=[]
+s_list=[]
+#put disease into the list
+for index in range (len(dict["Disease"])):
+    if not dict["Disease"][index] in d_list:
+        d_list.append(dict["Disease"][index])
 
-print("Total Rows:", row)
-print("Total Columns:", column)
-
-# printing the value of first column
-# Loop will print all values
-# of first column
-print("\nValue of first column")
-for i in range(1, row + 1):
-    cell_obj = sheet_obj.cell(row=i, column=1)
-    print(cell_obj.value)
-
-# printing the value of first column
-# Loop will print all values
-# of first row
-print("\nValue of first row")
-for i in range(1, column + 1):
-    cell_obj = sheet_obj.cell(row=2, column=i)
-    print(cell_obj.value, end=" ")
+for key in dict:
+    print(key)
